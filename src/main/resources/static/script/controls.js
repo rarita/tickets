589ee4a84adbd29f65e984a885b5f489cf0f89c5
annagr <grishanya1998@yandex.ru>
2020-04-30 20:03:13 +0300

    Выбор дат, типа перелета и количество взрослых / детей

--- src/main/resources/static/script/controls.js
+++ src/main/resources/static/script/controls.js
@@ -23,4 +23,46 @@ $(document).ready(function() {
             console.log("Selected " + ui.id + " - " + ui.value);
         }
     });
-})
+
+    // Это теперь инпут
+    $(".ar4_ast_2Xr6l").datepicker({
+        minDate: 0,
+        dateFormat: "d MM, yy",
+        monthNames : ['Январь','Февраль','Март','Апрель','Май','Июнь','Июль','Август','Сентябрь','Октябрь','Ноябрь','Декабрь'],
+        dayNamesMin : ['Вс','Пн','Вт','Ср','Чт','Пт','Сб']
+    });
+
+    $("#spinner_a").spinner({
+        max: 5,
+        min: 1
+    }).val(1);
+
+    $("#spinner_c").spinner({
+        max: 5,
+        min: 0
+    }).val(0);
+
+    $(".ui-spinner").css({
+        'float' : 'right'
+    });
+
+
+
+});
+
+/* When the user clicks on the button,
+        toggle between hiding and showing the dropdown content */
+function showDropdown() {
+    $('.ddown').css({
+        'display': 'block'
+    });
+}
+
+$(document).click(function(e) {
+        //if you click on anything except the modal itself or the "open modal" link, close the modal
+        if (!$(e.target).closest(".ddown,.aj6_apo_10ZKj").length) {
+            $('.ddown').css({
+                'display': 'none'
+            });
+        }
+});
