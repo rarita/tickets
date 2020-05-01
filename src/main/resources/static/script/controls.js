a278c1cd987b6504838dc7c91f9d1e2d859aeed8
annagr <grishanya1998@yandex.ru>
2020-05-01 14:38:24 +0300

    Вся главная страница кроме нерабочих дейтпикеров и марок (по горизонтали)

--- src/main/resources/static/script/controls.js
+++ src/main/resources/static/script/controls.js
@@ -1,9 +1,43 @@
 // on document ready
 
-$(document).ready(function() {
-    let cache = {};
+var itemWhere = null;
+var itemFrom = null;
 
-    $(".aqf_aqh_29PUo").autocomplete({
+// Кэш для autocomplete
+let cache = {};
+
+$(document).ready(function() {
+
+    assignAutocomplete($(".aqf_aqh_29PUo"));
+
+    // Это теперь инпут
+    assignDatepicker($(".ar4_ast_2Xr6l"));
+
+    $("#spinner_a").spinner({
+        max: 5,
+        min: 1
+    }).val(1);
+
+    $("#spinner_c").spinner({
+        max: 5,
+        min: 0
+    }).val(0);
+
+    $(".ui-spinner").css({
+        'float' : 'right'
+    });
+
+    $("#trip-type").change(function (e) {
+        if ($(this).val() === 'compound')
+            complexRouteGUI(true);
+        else
+            complexRouteGUI(false);
+    });
+
+});
+
+function assignAutocomplete(target) {
+    target.autocomplete({
         minLength: 2,
         source: function (request, response) {
             const term = request.term;
@@ -20,35 +54,24 @@ $(document).ready(function() {
 
         },
         select: function (event, ui) {
-            console.log("Selected " + ui.id + " - " + ui.value);
+            if (event.target.placeholder.startsWith('откуда:'))
+                itemFrom = ui.item;
+            else
+                itemWhere = ui.item;
+
+            console.dir(ui);
         }
     });
+}
 
-    // Это теперь инпут
-    $(".ar4_ast_2Xr6l").datepicker({
+function assignDatepicker(target) {
+    target.datepicker({
         minDate: 0,
         dateFormat: "d MM, yy",
         monthNames : ['Январь','Февраль','Март','Апрель','Май','Июнь','Июль','Август','Сентябрь','Октябрь','Ноябрь','Декабрь'],
         dayNamesMin : ['Вс','Пн','Вт','Ср','Чт','Пт','Сб']
     });
-
-    $("#spinner_a").spinner({
-        max: 5,
-        min: 1
-    }).val(1);
-
-    $("#spinner_c").spinner({
-        max: 5,
-        min: 0
-    }).val(0);
-
-    $(".ui-spinner").css({
-        'float' : 'right'
-    });
-
-
-
-});
+}
 
 /* When the user clicks on the button,
         toggle between hiding and showing the dropdown content */
@@ -56,6 +79,102 @@ function showDropdown() {
     $('.ddown').css({
         'display': 'block'
     });
+}
+
+function expandMarks() {
+    // Изменить высоту всех картинок-марок (тру быдлокод)
+    $(".mark_img").css({
+        'top': 530 + $(".m_form").length*50 + "px"
+    });
+}
+
+function cloneForm() {
+
+    // Клонируем и удаляем из DOM кнопку
+    let btn_add = $("#btn_add");
+    $(btn_add).detach();
+
+    // Клонируем форму
+    let form = $($(".m_form").get(0)).clone();
+
+    // Скопируем в новую форму dst старой
+    $(form).find("input").val("");
+    $(form.find("input").get(0)).val(itemWhere == null ? "" : itemWhere.value);
+
+    // Стили для кнопки
+    let btn = $(form).find(".Button_button_6a0ab")
+    $(btn).css({
+        'font-size' : '16px',
+        'color' : '#0b9e8a',
+        'background-color' : '#f1f2f6'
+    });
+
+    // Текст для кнопки
+    $((btn).find(".Button_contents_6a0ab")
+        .find("span")).text("Удалить перелет");
+
+    // Действие для кнопки
+    $(btn).attr("type", "button");
+    if ($(".m_form").length > 1) {
+
+        $(btn).css({
+            'font-size' : '16px',
+            'color' : 'white',
+            'background-color' : '#49a9aa'
+        });
+
+        $(btn).click(function () {
+            $(this).parent().parent().remove();
+
+            expandMarks();
+        });
+
+    }
+
+    form.appendTo("#holder");
+    btn_add.appendTo('#holder');
+    assignAutocomplete($(".aqf_aqh_29PUo"));
+    assignDatepicker($(form).find(".ar4_ast_2Xr6l"));
+
+    expandMarks();
+}
+
+// state - булеан true - on, false - off
+function complexRouteGUI(state) {
+    if (state) {
+        $("#MainHeroUsps-Usps").css({'display': 'none'}); // скрыть сердечко и текст
+
+        // Убираем второй дейтпикер и раздвигаем первый
+        $(".datepicker-2").css({'display': 'none'});
+        $(".ar4_ast_2Xr6l").css({'width': '100%'});
+
+        // Кнопка "Добавить ещё перелет"
+        let btn_add = $(".Button_button_6a0ab").clone();
+        $(btn_add).find("span").text("Добавить ещё перелет...");
+        $(btn_add).css({
+            'background-color': '#f1f2f6',
+            'color': '#14b5a3'
+        });
+        $(btn_add).attr("id", "btn_add");
+        $(btn_add).attr("type", "button");
+        $(btn_add).click(cloneForm);
+        $(btn_add).appendTo("#holder");
+
+        cloneForm(true);
+
+    }
+    else {
+        // Развернуть все обратно
+        $("#MainHeroUsps-Usps").css({'display': 'block'});
+        $(".datepicker-2").css({'display': 'inline-block'});
+        $(".ar4_ast_2Xr6l").css({'width': 'calc(100%-60px)'});
+
+        // Убрать лишние формы и контролы
+        $(".m_form").slice(1).remove();
+        $("#btn_add").remove();
+
+        expandMarks();
+    }
 }
 
 $(document).click(function(e) {
